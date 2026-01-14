8de62c48f673534f8b7aa3f0a6d2c802ab15810f
Mateo Zaballo <mzaballo@hotmail.com>
2026-01-14 11:34:19 -0300

    Avances

--- frontend/src/app/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
