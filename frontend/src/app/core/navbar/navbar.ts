c776173e48a8fa1d72ea6ad8e97cd3a88e4c7f27
Mateo Zaballo <mzaballo@hotmail.com>
2026-01-26 12:20:15 -0300

    nashe

--- frontend/src/app/core/navbar/navbar.ts
+++ frontend/src/app/core/navbar/navbar.ts
@@ -3,9 +3,18 @@ import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 
+import { LucideAngularModule, Mail, LucideIconProvider, LUCIDE_ICONS } from 'lucide-angular';
+
 @Component({
   selector: 'app-navbar',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, LucideAngularModule],
+  providers: [
+    {
+      provide: LUCIDE_ICONS,
+      multi: true,
+      useValue: new LucideIconProvider({ Mail }),
+    },
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
